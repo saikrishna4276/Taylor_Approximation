b36bcdb5fb8c05f000ad825cd79669f919b43e6e
“saikrishna4276” <avulasaikrishna@iitgn.ac.in>
2023-03-04 20:39:19 +0530

    added about a point

--- example.py
+++ example.py
@@ -1,24 +1,50 @@
 import streamlit as st
-from sympy import sin, cos, log, exp
-from sympy.abc import x
-st.title('Counter Example')
-
-# Streamlit runs from top to bottom on every iteraction so
-# we check if `count` has already been initialized in st.session_state.
-
-# If no, then initialize count to 0
-# If count is already initialized, don't do anything
+import matplotlib.pyplot as plt
+import jax.numpy as jnp
+from jax import grad
+# Store the initial value of widgets in session state
+from sympy import *
+from sympy.abc import X,x
+from sympy.parsing.sympy_parser import parse_expr
+from math import factorial
+st.set_option('deprecation.showPyplotGlobalUse', False)
 if 'count' not in st.session_state:
-	st.session_state.count = 0
-
-# Create a button which will increment the counter
-increment = st.button('Increment')
-if increment:
-    st.session_state.count += x
-
-# A button to decrement the counter
-decrement = st.button('Decrement')
-if decrement:
-    st.session_state.count -= cos(x)
-
-st.write('Count = ', st.session_state.count)
+    st.session_state.count = 0
+with st.sidebar:
+    z = parse_expr(st.text_input('Give an expession', 'sin(x)'))
+    z = lambdify(x,z,"jax")
+    if st.button("sin(x)"):
+        st.session_state.count+=sin(X)
+    if st.button("cos(x)"):
+        st.session_state.count+=cos(X)
+    if st.button("log(1+x)"):
+        st.session_state.count+=log(1+X)
+    if st.button("log(1-x)"):
+        st.session_state.count+=log(1-X)
+    if st.button("exp(x)"):
+        st.session_state.count+=exp(X)
+    if st.button('RESET'):
+        st.session_state.count=0
+    st.text(st.session_state.count)
+def f(a):
+    global z
+    return z(a)
+def taylor(fun, degree, x_plot, a=0.):
+    y = fun(a)
+    #st.write(y)
+    gradient = grad(fun)
+    for i in range(1,degree+1):
+        y += (gradient(a)*(x_plot-a)**i)/factorial(i)
+        gradient = grad(gradient)
+    return y
+x_plot = jnp.linspace(-jnp.pi, jnp.pi, 100)
+with st.sidebar:
+    # Create a slider for degree
+    degree = st.slider("Degree", 1, 15, 1)
+approximation = taylor(f,degree,x_plot)
+#st.write(approximation)
+x = jnp.linspace(-3,3,100)
+fig, ax = plt.subplots()
+ax.plot(x,z(x_plot))
+ax.plot(x,approximation)
+st.pyplot(fig)
